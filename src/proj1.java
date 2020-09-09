d9b2c4427eb3126b31d9fd58f313d808314e8fbf
unknown <chacetrev10@gmail.com>
2020-09-09 12:22:17 -0600

    Initial commit with zookeeper framework

---
+++ src/proj1.java
@@ -0,0 +1,53 @@
+public class proj1 {
+
+    public static void main(String[] args){
+
+    }
+}
+
+//Zoo employee class that contains methods that all zoo employees do
+abstract class ZooEmployee{
+
+    abstract void goToWork(String day);
+
+    abstract void wakeUpAnimal(String name);
+
+    abstract void sleepAnimal(String name);
+
+    void countAnimal(String name){
+        System.out.println(name + " is counted for roll call");
+    }
+
+    void leaveZoo(String day){
+        System.out.println("Employee leaves zoo on day " + day);
+    }
+}
+
+//Provides implementation for abstract method inherited and has its own methods specific to a zookeeper
+class Zookeeper extends ZooEmployee{
+
+    void goToWork(String day){
+        System.out.println("Zookeeper arrives to zoo on day " + day);
+    }
+
+    void wakeUpAnimal(String name){
+        System.out.println("Zookeeper wakes up " + name);
+    }
+
+    void sleepAnimal(String name){
+        System.out.println("Zookeeper tells " + name + " to go to bed");
+    }
+
+    void feedAnimal(String name){
+        System.out.println("Zookeeper feeds "+ name);
+    }
+
+    void exerciseAnimal(String name){
+        System.out.println("Zookeeper exercises "+ name);
+    }
+
+    @Override
+    void leaveZoo(String day) {
+        System.out.println("Zookeeper leaves zoo on day " + day);
+    }
+}
