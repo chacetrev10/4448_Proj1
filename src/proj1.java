213ad29547a0a7719d24311d01e48790bbc556d3
unknown <chacetrev10@gmail.com>
2020-09-12 11:10:56 -0600

    added basic animal class structure

--- src/proj1.java
+++ src/proj1.java
@@ -2,56 +2,56 @@ import java.util.Scanner;
 
 public class proj1 {
 
-    public static void main(String[] args){
-        System.out.println("Please enter the number of days you want to simulate at the zoo");
-        Scanner input = new Scanner(System.in);
-        int numOfDays = input.nextInt();
-    }
+	public static void main(String[] args) {
+		System.out.println("Please enter the number of days you want to simulate at the zoo");
+		Scanner input = new Scanner(System.in);
+		int numOfDays = input.nextInt();
+	}
 }
 
 //Zoo employee class that contains methods that all zoo employees do
-abstract class ZooEmployee{
+abstract class ZooEmployee {
 
-    abstract void goToWork(String day);
+	abstract void goToWork(String day);
 
-    abstract void wakeUpAnimal(String name);
+	abstract void wakeUpAnimal(String name);
 
-    abstract void sleepAnimal(String name);
+	abstract void sleepAnimal(String name);
 
-    void countAnimal(String name){
-        System.out.println(name + " is counted for roll call");
-    }
+	void countAnimal(String name) {
+		System.out.println(name + " is counted for roll call");
+	}
 
-    void leaveZoo(String day){
-        System.out.println("Employee leaves zoo on day " + day);
-    }
+	void leaveZoo(String day) {
+		System.out.println("Employee leaves zoo on day " + day);
+	}
 }
 
 //Provides implementation for abstract method inherited and has its own methods specific to a zookeeper
-class Zookeeper extends ZooEmployee{
+class Zookeeper extends ZooEmployee {
 
-    void goToWork(String day){
-        System.out.println("Zookeeper arrives to zoo on day " + day);
-    }
+	void goToWork(String day) {
+		System.out.println("Zookeeper arrives to zoo on day " + day);
+	}
 
-    void wakeUpAnimal(String name){
-        System.out.println("Zookeeper wakes up " + name);
-    }
+	void wakeUpAnimal(String name) {
+		System.out.println("Zookeeper wakes up " + name);
+	}
 
-    void sleepAnimal(String name){
-        System.out.println("Zookeeper tells " + name + " to go to bed");
-    }
+	void sleepAnimal(String name) {
+		System.out.println("Zookeeper tells " + name + " to go to bed");
+	}
 
-    void feedAnimal(String name){
-        System.out.println("Zookeeper feeds "+ name);
-    }
+	void feedAnimal(String name) {
+		System.out.println("Zookeeper feeds " + name);
+	}
 
-    void exerciseAnimal(String name){
-        System.out.println("Zookeeper exercises "+ name);
-    }
+	void exerciseAnimal(String name) {
+		System.out.println("Zookeeper exercises " + name);
+	}
 
-    @Override
-    void leaveZoo(String day) {
-        System.out.println("Zookeeper leaves zoo on day " + day);
-    }
+	@Override
+	void leaveZoo(String day) {
+		System.out.println("Zookeeper leaves zoo on day " + day);
+	}
 }
